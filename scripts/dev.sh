b2eaabbf677d009f97a50a4351f7f347ac5eeda3
Zoltan Groschwitz <zoltan.groschwitz@rosenbauer.com>
2026-04-07 15:40:38 +0200

    Adapt workspace

--- scripts/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-HERE="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-
-"$HERE/node22.sh" npm ci
-rm -rf "$HERE/../.angular/cache" || true
-"$HERE/node22.sh" npm start
-
